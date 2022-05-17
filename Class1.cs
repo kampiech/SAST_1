f62a70db35cbbf820d6d76c951bd4ffb2380978e
Kamil Piechociak <kamil.piechociak@pracuj.pl>
2022-05-17 10:21:57 +0200

    init

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace SAST
+{
+    public class Class1
+    {
+        //commands in PATH is security-sensitive
+        private void CommandsPath()
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "binary"; // Sensitive
+        }
+    }
+}
