c1034991db4258914c7679078d208422e6fb35d2
Kamil Piechociak <kamil.piechociak@pracuj.pl>
2022-05-19 10:30:10 +0200

    ts

---
+++ file_1.ts
@@ -0,0 +1,15 @@
+﻿//Example of DOM open redirect vulnerability (http://vulnerable/page.html#https://www.attacker.com/):
+document.location = document.location.hash.slice(1);
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: "admin",
+        database: "project",
+        password: "mypassword", // sensitive
+        multipleStatements: true
+    });
+
+connection.connect();
